3af094fe6dee9b929f1b6981a8ada2f3eef4391a
Séverin Lemaignan <severin.lemaignan@pal-robotics.com>
2022-10-18 12:06:52 +0200

    Rebase from 'debian/noetic/pyhri'

--- src/hri/face.py
+++ src/hri/face.py
@@ -1,10 +1,47 @@
-from typing import Optional
-import numpy.typing as npt
+# Copyright 2022 PAL Robotics S.L.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#    * Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#
+#    * Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in the
+#      documentation and/or other materials provided with the distribution.
+#
+#    * Neither the name of the PAL Robotics S.L. nor the names of its
+#      contributors may be used to endorse or promote products derived from
+#      this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
 
-import rospy
-from sensor_msgs.msg import RegionOfInterest, Image
-from hri_msgs.msg import FacialLandmarks, SoftBiometrics
-from cv_bridge import CvBridge
+from typing import Optional
+
+try:
+    import rospy
+    from sensor_msgs.msg import RegionOfInterest, Image
+    from hri_msgs.msg import FacialLandmarks, SoftBiometrics
+    from cv_bridge import CvBridge
+    from geometry_msgs.msg import TransformStamped
+
+    from tf2_ros import LookupException
+
+    FACE_TF_TIMEOUT = rospy.Duration(0.01)
+except ImportError:
+    print(
+        "Importing pyhri without rospy! This won't work (except for generating documentation)"
+    )
 
 
 class Rect:
@@ -16,17 +53,26 @@ class Rect:
 
 
 class Face:
+    """Represents a detected face."""
+
     def __init__(self, id, tf_buffer, reference_frame):
         self.id = id
         self.ns = "/humans/faces/" + id
+        self.frame = "face_" + id
+        self.gaze_frame = "gaze_" + id
+
+        self._valid = True
 
         self.roi: Optional[Rect] = None
-        self.cropped: Optional[npt.ArrayLike] = None
-        self.aligned: Optional[npt.ArrayLike] = None
+        self.cropped = None
+        self.aligned = None
         self.landmarks: Optional[FacialLandmarks] = None
         self.softbiometrics: Optional[SoftBiometrics] = None
 
         self.cv_bridge = CvBridge()
+
+        self._tf_buffer = tf_buffer
+        self._reference_frame = reference_frame
 
         rospy.logdebug("New face detected: " + self.ns)
 
@@ -49,11 +95,18 @@ class Face:
         )
 
     def close(self):
+        self._valid = False
         self.roi_sub.unregister()
         self.cropped_sub.unregister()
         self.aligned_sub.unregister()
         self.landmarks_sub.unregister()
         self.softbiometrics_sub.unregister()
+
+    def valid(self) -> bool:
+        """Returns True if this face still exists (and thus is valid).
+        If False, methods like `Face.transform` will raise an exception.
+        """
+        return self._valid
 
     def on_roi(self, msg):
         self.roi = Rect(msg.x_offset, msg.y_offset, msg.width, msg.height)
@@ -69,6 +122,54 @@ class Face:
 
     def on_softbiometrics(self, msg):
         self.softbiometrics = msg
+
+    def transform(self, from_frame=None):
+        """Returns a ROS TransformStamped of the face, from the `from_frame` reference basis.
+        If `from_frame` is not provided, uses the default `reference_frame` (usually `base_link`).
+        """
+
+        if from_frame is None:
+            from_frame = self._reference_frame
+
+        try:
+            return self._tf_buffer.lookup_transform(
+                from_frame, self.frame, rospy.Time(0), FACE_TF_TIMEOUT
+            )
+
+        except LookupException:
+            rospy.logwarn(
+                "failed to transform face frame "
+                + self.frame
+                + " to "
+                + from_frame
+                + ". Are the frames published?"
+            )
+
+            return TransformStamped()
+
+    def gaze_transform(self, from_frame=None):
+        """Returns a ROS TransformStamped of the face's gaze, from the `from_frame` reference basis.
+        If `from_frame` is not provided, uses the default `reference_frame` (usually `base_link`).
+        """
+
+        if from_frame is None:
+            from_frame = self._reference_frame
+
+        try:
+            return self._tf_buffer.lookup_transform(
+                from_frame, self.gaze_frame, rospy.Time(0), FACE_TF_TIMEOUT
+            )
+
+        except LookupException:
+            rospy.logwarn(
+                "failed to transform gaze frame "
+                + self.gaze_frame
+                + " to "
+                + from_frame
+                + ". Are the frames published?"
+            )
+
+            return TransformStamped()
 
     def __str__(self):
         return self.id
--- src/hri/person.py
+++ src/hri/person.py
@@ -1,21 +1,61 @@
+# Copyright 2022 PAL Robotics S.L.
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions are met:
+#
+#    * Redistributions of source code must retain the above copyright
+#      notice, this list of conditions and the following disclaimer.
+#
+#    * Redistributions in binary form must reproduce the above copyright
+#      notice, this list of conditions and the following disclaimer in the
+#      documentation and/or other materials provided with the distribution.
+#
+#    * Neither the name of the PAL Robotics S.L. nor the names of its
+#      contributors may be used to endorse or promote products derived from
+#      this software without specific prior written permission.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
 from typing import Optional
 
-import rospy
-from std_msgs.msg import String, Bool, Float32
-from hri_msgs.msg import EngagementLevel
-from geometry_msgs.msg import TransformStamped
+try:
+    import rospy
+    from std_msgs.msg import String, Bool, Float32
+    from hri_msgs.msg import EngagementLevel
+    from geometry_msgs.msg import TransformStamped
 
-from tf2_ros import LookupException
+    from tf2_ros import LookupException
 
-
-PERSON_TF_TIMEOUT = rospy.Duration(0.01)
+    PERSON_TF_TIMEOUT = rospy.Duration(0.01)
+except ImportError:
+    print(
+        "Importing pyhri without rospy! This won't work (except for generating documentation)"
+    )
 
 
 class Person:
+    """Represents a known person (that can be currently tracked or not).
+
+    A :class:`Person` instance gives access to its corresponding :class:`Face`,
+    :class:`Body` and :class:`Voice` if available.
+    """
+
     def __init__(self, id, tf_buffer, reference_frame):
-        self.id = id
-        self.ns = "/humans/persons/" + id
-        self.frame = "person_" + id
+        self.id = id  #: the person ID
+        self.ns = "/humans/persons/" + id  #: the person namespace
+        self.frame = "person_" + id  #: the TF frame of the person
+
+        self._valid = True
 
         # these 3 member variables are set when the Person instance is constructed in hri.py
         self.faces_ = {}
@@ -25,16 +65,26 @@ class Person:
         from . import Face, Body, Voice
 
         self.face_id: Optional[str] = None
-        self.face: Optional[Face] = None
+        self.face: Optional[
+            Face
+        ] = None  #: access to the person's face (:class:`Face` instance), if available
         self.body_id: Optional[str] = None
-        self.body: Optional[Body] = None
+        self.body: Optional[
+            Body
+        ] = None  #: access to the person's body (:class:`Body` instance), if available
         self.voice_id: Optional[str] = None
-        self.voice: Optional[Voice] = None
+        self.voice: Optional[
+            Voice
+        ] = None  #: access to the person's voice (:class:`Voice` instance), if available
 
-        self.anonymous: bool = False
-        self.engagement_status: Optional[EngagementLevel] = None
-        self.alias: Optional[str] = None
-        self.loc_confidence: float = 0.0
+        self.anonymous: bool = False  #: whether or not this person is anonymous
+        self.engagement_status: Optional[
+            EngagementLevel
+        ] = None  #: current estimated level of engagement
+        self.alias: Optional[
+            str
+        ] = None  #: if non-null, the person this person is an alias of
+        self.loc_confidence: float = 0.0  #: level of confidence of the person's location (0.0: no confidence, 1.0: person currently seen)
 
         self._tf_buffer = tf_buffer
         self._reference_frame = reference_frame
@@ -68,6 +118,7 @@ class Person:
         )
 
     def close(self):
+        self._valid = False
         self.face_id_sub.unregister()
         self.body_id_sub.unregister()
         self.voice_id_sub.unregister()
@@ -75,6 +126,12 @@ class Person:
         self.alias_sub.unregister()
         self.engagement_sub.unregister()
         self.loc_confidence_sub.unregister()
+
+    def valid(self) -> bool:
+        """Returns True if this person still exists (and thus is valid).
+        If False, methods like `Person.transform` will raise an exception.
+        """
+        return self._valid
 
     def on_face_id(self, msg):
         if msg.data and msg.data in self.faces_:
@@ -107,19 +164,10 @@ class Person:
         self.alias = msg.data
 
     def on_engagement_status(self, msg):
-        self.engagement_status = msg.data
+        self.engagement_status = msg.level
 
     def on_loc_confidence(self, msg):
         self.loc_confidence = msg.data
-
-    def face(self):
-        pass
-
-    def body(self):
-        pass
-
-    def voice(self):
-        pass
 
     def engagement_level(self):
         if self.engagement_status is None:
@@ -132,7 +180,7 @@ class Person:
             return TransformStamped()
 
         try:
-            return self._tf_buffer.lookupTransform(
+            return self._tf_buffer.lookup_transform(
                 self._reference_frame, self.frame, rospy.Time(0), PERSON_TF_TIMEOUT
             )
 
