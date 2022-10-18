2a0376d2355c1a732f27669f4fcff541218cbf81
Séverin Lemaignan <severin.lemaignan@pal-robotics.com>
2022-10-18 12:06:41 +0200

    Rebase from 'debian/noetic/pyhri'

--- CHANGELOG.rst
+++ CHANGELOG.rst
@@ -2,6 +2,30 @@
 Changelog for package pyhri
 ^^^^^^^^^^^^^^^^^^^^^^^^^^^
 
+0.2.0 (2022-10-18)
+------------------
+* autogenerate docs with rosdoc (for display on ROS wiki) and SPHINX
+* More api documentation
+* adjust code base for ReadTheDocs doc generation
+  In particular:
+  - add a requirements.txt file for catkin
+  - allows importing pyhri without rospy
+* Face.transform|Face.gaze_transform can change their reference frame
+* removed dep on numpy.typing as it is not widely available yet
+* mark features as invalid once they disappear
+* returns copy of persons to avoid modifying dictionary while iterating
+  While here, add infrastrcutre to check whether a face/body/voice/person is still valid
+* expose the tf transform of the face and gaze
+* add BSD license file
+* [test] further tuning of tests' waiting behaviours for #1
+* Contributors: Séverin Lemaignan
+
+0.1.3 (2022-07-13)
+------------------
+* refining waiting + closing in tests -- they all pass reliably on my machine
+* unregister from main topics when closing HRIListener
+* Contributors: Séverin Lemaignan
+
 0.1.2 (2022-07-12)
 ------------------
 * [cmake] add test only behing CATKIN_ENABLE_TESTING
--- src/hri/hri.py
+++ src/hri/hri.py
@@ -28,18 +28,29 @@
 
 from typing import Mapping
 
-import rospy
 
 from .body import Body
 from .face import Face
 from .voice import Voice
 from .person import Person
-from hri_msgs.msg import IdsList
 
-from tf2_ros import Buffer, TransformListener
+try:
+    import rospy
+    from hri_msgs.msg import IdsList
+    from tf2_ros import Buffer, TransformListener
+except ImportError:
+
+    class IdsList:
+        pass
+
+    print(
+        "Importing pyhri without rospy! This won't work (except for generating documentation)"
+    )
 
 
 class HRIListener:
+    """Main entry point to pyhri."""
+
     def __init__(self, reference_frame="base_link"):
 
         self.feature_subscribers_ = {}
@@ -49,18 +60,22 @@ class HRIListener:
         self.face_lost_callbacks = []
 
         self.bodies: Mapping[str, Body] = {}
+
         self.body_callbacks = []
         self.body_lost_callbacks = []
 
         self.voices: Mapping[str, Voice] = {}
+
         self.voice_callbacks = []
         self.voice_lost_callbacks = []
 
         self.tracked_persons: Mapping[str, Person] = {}
+
         self.person_tracked_callbacks = []
         self.person_tracked_lost_callbacks = []
 
         self.known_persons: Mapping[str, Person] = {}
+
         self.known_person_callbacks = []
         self.known_person_lost_callbacks = []
 
@@ -73,23 +88,36 @@ class HRIListener:
 
         self._subscribers = {
             "face": rospy.Subscriber(
-                "/humans/faces/tracked", IdsList, self.on_tracked_faces
+                "/humans/faces/tracked", IdsList, self._on_tracked_faces
             ),
             "body": rospy.Subscriber(
-                "/humans/bodies/tracked", IdsList, self.on_tracked_bodies
+                "/humans/bodies/tracked", IdsList, self._on_tracked_bodies
             ),
             "voice": rospy.Subscriber(
-                "/humans/voices/tracked", IdsList, self.on_tracked_voices
+                "/humans/voices/tracked", IdsList, self._on_tracked_voices
             ),
             "tracked_persons": rospy.Subscriber(
-                "/humans/persons/tracked", IdsList, self.on_tracked_persons
+                "/humans/persons/tracked", IdsList, self._on_tracked_persons
             ),
             "known_persons": rospy.Subscriber(
-                "/humans/persons/known", IdsList, self.on_known_persons
+                "/humans/persons/known", IdsList, self._on_known_persons
             ),
         }
 
-    def update_trackers(self, tracker, tracker_class, new_ids):
+    def __del__(self):
+        self.close()
+
+    def close(self):
+
+        if self._subscribers:
+            for _, sub in self._subscribers.items():
+                sub.unregister()
+
+            self._subscribers = {}
+
+            rospy.logdebug("HRI listener closed")
+
+    def _update_trackers(self, tracker, tracker_class, new_ids):
 
         new_ids = set(new_ids)
         current_ids = set(tracker.keys())
@@ -111,99 +139,124 @@ class HRIListener:
                 tracker[id].bodies_ = self.bodies
                 tracker[id].voices_ = self.voices
 
-    def on_tracked_faces(self, tracked: IdsList):
+    def _on_tracked_faces(self, tracked: IdsList):
 
-        self.update_trackers(self.faces, Face, tracked.ids)
+        self._update_trackers(self.faces, Face, tracked.ids)
 
-    def on_tracked_bodies(self, tracked: IdsList):
+    def _on_tracked_bodies(self, tracked: IdsList):
 
-        self.update_trackers(self.bodies, Body, tracked.ids)
+        self._update_trackers(self.bodies, Body, tracked.ids)
 
-    def on_tracked_voices(self, tracked: IdsList):
+    def _on_tracked_voices(self, tracked: IdsList):
 
-        self.update_trackers(self.voices, Voice, tracked.ids)
+        self._update_trackers(self.voices, Voice, tracked.ids)
 
-    def on_tracked_persons(self, tracked: IdsList):
+    def _on_tracked_persons(self, tracked: IdsList):
 
-        self.update_trackers(self.tracked_persons, Person, tracked.ids)
+        self._update_trackers(self.tracked_persons, Person, tracked.ids)
 
-    def on_known_persons(self, tracked: IdsList):
+    def _on_known_persons(self, tracked: IdsList):
 
-        self.update_trackers(self.known_persons, Person, tracked.ids)
+        self._update_trackers(self.known_persons, Person, tracked.ids)
 
-    # Registers a callback function, to be invoked everytime a new face
-    # is detected.
+    def get_faces(self) -> Mapping[str, Face]:
+        """Returns the list of all currently tracked faces, mapped to their ID."""
+        return self.faces.copy()
+
     def on_face(self, callback):
+        """Registers a callback function, to be invoked everytime a new face
+        is detected."""
         self.face_callbacks.append((callback))
 
-    # Registers a callback function, to be invoked everytime a
-    # previously tracked face is lost (eg, not detected anymore)
     def on_face_lost(self, callback):
+        """Registers a callback function, to be invoked everytime a
+        previously tracked face is lost (eg, not detected anymore)
+        """
         self.face_lost_callbacks.append((callback))
 
-    # Registers a callback function, to be invoked everytime a new body
-    # is detected.
+    def get_bodies(self) -> Mapping[str, Body]:
+        """Returns the list of all currently tracked bodies, mapped to their ID."""
+        return self.bodies.copy()
+
     def on_body(self, callback):
+        """Registers a callback function, to be invoked everytime a new body
+        is detected.
+        """
         self.body_callbacks.append((callback))
 
-    # Registers a callback function, to be invoked everytime a
-    # previously tracked body is lost (eg, not detected anymore)
     def on_body_lost(self, callback):
+        """Registers a callback function, to be invoked everytime a
+        previously tracked body is lost (eg, not detected anymore)
+        """
         self.body_lost_callbacks.append((callback))
 
-    # Registers a callback function, to be invoked everytime a new voice
-    # is detected.
+    def get_voices(self) -> Mapping[str, Voice]:
+        """Returns the list of all currently 'tracked' voices, mapped to their ID."""
+        return self.voices.copy()
+
     def on_voice(self, callback):
+        """Registers a callback function, to be invoked everytime a new voice
+        is detected.
+        """
         self.voice_callbacks.append((callback))
 
-    # Registers a callback function, to be invoked everytime a
-    # previously tracked voice is lost (eg, not detected anymore)
     def on_voice_lost(self, callback):
+        """Registers a callback function, to be invoked everytime a
+        previously tracked voice is lost (eg, not detected anymore)
+        """
         self.voice_lost_callbacks.append((callback))
 
-    # Returns the list of all known persons, whether or not they are
-    # currently actively detected (eg, seen). The persons are mapped to their
-    # IDs.
-    #
-    # Persons are returned as constant std::weak_ptr: while person do *not*
-    # disappear in general, *anonymous* persons (created because, eg, a face has
-    # been detected, and we can infer a yet-to-be-recognised person does exist)
-    # can disappear.
     def get_persons(self) -> Mapping[str, Person]:
-        return self.known_persons
+        """Returns the list of all known persons, whether or not they are
+        currently actively detected (eg, seen). The persons are mapped to their
+        IDs.
 
-    # Registers a callback function, to be invoked everytime a new person
-    # is detected.
+        While person do *not* disappear in general, *anonymous* persons (created
+        because, eg, a face has been detected, and we can infer a
+        yet-to-be-recognised person does exist) can disappear.  Use the `valid()`
+        method to ensure the person still exists.
+        """
+        return self.known_persons.copy()
+
     def on_person(self, callback):
+        """Registers a callback function, to be invoked everytime a new person
+        is detected.
+        """
         self.known_person_callbacks.append((callback))
 
-    # Registers a callback function, to be invoked everytime a person
-    # is lost. This can *only* happen for anonymous persons. Identified persons
-    # will never be removed from the list of all known persons.
     def on_person_lost(self, callback):
+        """Registers a callback function, to be invoked everytime a person
+        is lost. This can *only* happen for anonymous persons. Identified persons
+        will never be removed from the list of all known persons.
+        """
         self.known_person_lost_callbacks.append((callback))
 
-    # Returns the list of currently detected persons, mapped to their IDs
-    #
-    # Note that, while person do *not* disappear in general, *anonymous*
-    # persons (created because, eg, a face has been detected, and we can infer
-    # a yet-to-be-recognised person does exist) can disappear.
     def get_tracked_persons(self) -> Mapping[str, Person]:
-        return self.tracked_persons
+        """Returns the list of currently detected persons, mapped to their IDs
 
-    # Registers a callback function, to be invoked everytime a new person
-    # is detected and actively tracked (eg, currently seen).
+        Note that, while person do *not* disappear in general, *anonymous*
+        persons (created because, eg, a face has been detected, and we can infer
+        a yet-to-be-recognised person does exist) can disappear.
+        Use the `valid()` method to ensure the person still exists.
+        """
+        return self.tracked_persons.copy()
+
     def on_tracked_person(self, callback):
+        """Registers a callback function, to be invoked everytime a new person
+        is detected and actively tracked (eg, currently seen).
+        """
         self.person_tracked_callbacks.append((callback))
 
-    # Registers a callback function, to be invoked everytime a previously tracked
-    # person is lost.
     def on_tracked_person_lost(self, callback):
+        """Registers a callback function, to be invoked everytime a previously tracked
+        person is lost.
+        """
         self.person_tracked_lost_callbacks.append((callback))
 
-    # sets the reference frame from which the TF transformations of the persons will
-    # be returned (via `Person::transform()`).
-    #
-    # By default, `base_link`.
     def set_reference_frame(frame):
+        """Sets the reference frame from which the TF transformations of the persons will
+        be returned (via `Person::transform()`).
+
+        By default, `base_link`.
+        """
         self._reference_frame = frame
